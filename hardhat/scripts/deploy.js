43e57cd78f38aecb760cae9dcd552ed0b02f1211
Mehdi <rhouzm@gmail.com>
2022-09-18 10:12:59 +0200

    feat : hardhat

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+
+async function main() {
+  
+  const Oracle = await hre.ethers.getContractFactory("");
+  const oracle= await oracle.deploy();
+  await oracle.deployed();
+
+  const Consumer = await hre.ethers.getContractFactory("ATestnetConsumer");
+  const consumer= await Consumer.deploy();
+  await consumer.deployed();
+
+  console.log("Consumer deployed to", consumer.address)  
+  console.log("Oracle deployed to", oracle.address)
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
