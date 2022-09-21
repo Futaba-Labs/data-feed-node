66e664d0ee3b3dce43eb5f9b8e8e0a88ecb04a78
mehdi <rhouzm@gmail.com>
2022-09-21 16:10:54 +0200

    enhancement phase 3/6 : encode ✔️ , loading [] by timestamp

--- quicknode/scanBlock.py
+++ quicknode/scanBlock.py
@@ -6,9 +6,6 @@ from web3.exceptions import BlockNotFound
 '''
 This is a wrapper to scan block by timestamp.
 It uses bisection search to look for a block based on timestamp with cuttoffs of areas.
+TO DO 
 '''
-
-
-
-
 
